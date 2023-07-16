8b46885452dd657718d6216ab046b5de66447199
Thiago Santa Clara <strovsk@outlook.com>
2023-07-16 15:42:27 -0300

    tests: include test log drivers operations (add and remove)
    refactor: sort imports
    refactor: change default set logger name from root_logger and use a classmethod instead

--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -1,6 +1,8 @@
-from src.Log import Log
-import pytest
 import logging
+
+import pytest
+
+from src.Log import Log
 
 
 @pytest.fixture
@@ -15,7 +17,7 @@ def test_log_should_be_a_singleton(log_obj: Log):
 def test_log_can_change_name():
     default_logger_name = Log.log_name()
     new_logger_name = "my_logger_test"
-    Log.root_logger.name = new_logger_name
+    Log.set_logger_name(new_logger_name)
     assert Log.log_name() == new_logger_name and Log.log_name() != default_logger_name
 
 
@@ -36,6 +38,44 @@ def test_log_cannot_add_same_driver_twice():
     assert number_of_current_drivers == 1 and number_of_default_drivers == 0
 
 
+def test_log_cannot_add_unexistent_driver():
+    with pytest.raises(Exception):
+        Log.add_driver("BadDriver")
+
+
+def test_log_can_add_generic_driver():
+    class CustomGenericHandler(logging.Handler):
+        ...
+
+    Log.add_driver(
+        "newDriver", driver_func=lambda driver_option: CustomGenericHandler()
+    )
+
+    log_handlers = Log.root_logger.handlers
+    assert (
+        type(log_handlers[0]).__name__ == "CustomGenericHandler"
+        and len(log_handlers) == 1
+    )
+
+
+def test_log_can_remove_generic_driver():
+    class CustomGenericHandler(logging.Handler):
+        ...
+
+    Log.add_driver(
+        "newDriver", driver_func=lambda driver_option: CustomGenericHandler()
+    )
+
+    Log.add_driver("file")
+    Log.remove_driver("file")
+
+    stream_type = Log.drivers["newDriver"](None)
+    Log.remove_driver("newDriver")
+    log_drivers = Log.drivers.keys()
+    current_drivers = Log.root_logger.handlers
+    assert len(current_drivers) == 0
+
+
 def test_log_can_right_info(mocker):
     mocker.patch("logging.Logger.info")
     Log.info("message")
@@ -52,10 +92,12 @@ def test_log_can_right_info(mocker):
 def test_log_can_right_warning(mocker):
     mocker.patch("logging.Logger.warning")
     Log.warn("message")
+    Log.warning("message")
     assert logging.Logger.warning.assert_called_with("message", extra=None) is None
 
     custom_extra = {"custom": "value"}
     Log.warn("another message", extra=custom_extra)
+    Log.warning("another message", extra=custom_extra)
     assert (
         logging.Logger.warning.assert_called_with("another message", extra=custom_extra)
         is None
