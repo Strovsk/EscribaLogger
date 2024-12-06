8f465f3cefa125ed44f708b30fa36b8ef91cabc5
strovsk <strovsk@outlook.com>
2024-12-06 00:28:47 -0300

    feat: enhance process_extra_context to handle class instances and improve string representation

--- src/EscribaLogger/process_extra_context.py
+++ src/EscribaLogger/process_extra_context.py
@@ -1,5 +1,11 @@
 import json
+import inspect
 
+def transform_class_into_string(value):
+    result = str(value)
+    class_params = inspect.signature(value.__init__).parameters
+    class_params = list(class_params.keys())
+    return result
 
 def process_extra_context(context: dict):
     if not context:
@@ -8,4 +14,13 @@ def process_extra_context(context: dict):
         raise TypeError(
             f"Context must be a object dict, not {type(context).__name__}!!!"
         )
+
+    for key, value in context.items():
+        if isinstance(value, type):
+            context.update({key: transform_class_into_string(value)})
+        elif hasattr(value, '__str__'):
+            class_name = value.__class__.__name__
+            new_value = f"<object ({class_name})>: {str(value)}"
+            context.update({key: new_value})
+
     return " - " + json.dumps(context)
