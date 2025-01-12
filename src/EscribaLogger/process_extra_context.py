a12f1f22ddf4281a552bc5683af4c91d338b766e
Thiago Santa Clara <strovsk@outlook.com>
2025-01-12 12:22:42 -0300

    test: fix some tests

--- src/EscribaLogger/process_extra_context.py
+++ src/EscribaLogger/process_extra_context.py
@@ -1,11 +1,5 @@
 import json
-import inspect
 
-def transform_class_into_string(value):
-    result = str(value)
-    class_params = inspect.signature(value.__init__).parameters
-    class_params = list(class_params.keys())
-    return result
 
 def process_extra_context(context: dict):
     if not context:
@@ -16,9 +10,9 @@ def process_extra_context(context: dict):
         )
 
     for key, value in context.items():
-        if isinstance(value, type):
-            context.update({key: transform_class_into_string(value)})
-        elif hasattr(value, '__str__'):
+        if isinstance(value, (str, int, float, bool, list, dict, tuple, set)):
+            continue
+        elif hasattr(value, "__str__"):
             class_name = value.__class__.__name__
             new_value = f"<object ({class_name})>: {str(value)}"
             context.update({key: new_value})
--- tests/unit/test_process_extra_context.py
+++ tests/unit/test_process_extra_context.py
@@ -1,18 +1,23 @@
-from EscribaLogger.process_extra_context import process_extra_context
 import re
+
+from EscribaLogger.process_extra_context import process_extra_context
+
 
 def test_should_process_extra_context():
     sample_dict = {"key": "value"}
     result = process_extra_context(sample_dict)
     assert result == ' - {"key": "value"}'
 
+
 def test_should_return_empty_string_when_empty_context():
     result = process_extra_context({})
     assert result == ""
 
+
 def test_should_return_empty_string_when_context_is_none():
     result = process_extra_context(None)
     assert result == ""
+
 
 def test_should_convert_object_instances_to_string():
     class Item:
@@ -24,9 +29,11 @@ def test_should_convert_object_instances_to_string():
         def __str__(self):
             return "the Escriba"
 
-    sample_dict = {"full_class": Item, 'object': Item()}
+    sample_dict = {"full_class": Item, "object": Item()}
     result = process_extra_context(sample_dict)
 
-    assert type(result) == str
-    assert re.search(r'\.Item', result)
-    assert re.search(r'<object \(Item\)\>: the Escriba', result)
+    assert type(result) is str
+    assert re.search(r"\.Item", result)
+    assert re.search(r"<object \(Item\)\>: the Escriba", result)
+    assert re.search(r"\.Item", result)
+    assert re.search(r"<object \(Item\)\>: the Escriba", result)
